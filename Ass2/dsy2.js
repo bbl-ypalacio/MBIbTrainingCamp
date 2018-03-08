112e70eaa0e1c88047a6e9c051631ff6a85de5bd
bblypalacio <ypalacio@belizebank.com>
2018-03-08 13:46:38 -0600

    Pushing day 2 assigments

--- Ass2/dsy2.js
+++ Ass2/dsy2.js
@@ -1,2 +1,40 @@
-// JavaScript source yrp
-console.log('\nBegin---> The Calculator'); 
+//Assigment 2
+var wl = ['J', 'U', 'M', 'P'];
+var gl = ['_', '_', '_', '_'];
+
+var gAttemps    = 0 , bAttemps = 0;
+var gAttempsArr = [], bAttempsArr = [];
+
+function guessLetter(letter) {
+
+    var gg  = false;
+    var mtg = false;
+
+    for (var i = 0; i < wl.length; i++) {
+        if (wl[i] == letter) {
+            gl[i] = letter;
+            gg = true;
+        }
+        if (gl[i] == '_') {
+            mtg = true;
+        }
+    }
+    if (gg) {
+        console.log('Letter Found!');
+        console.log(gl.join(''));
+        if (!mtg) {
+            console.log('Confratulation!');
+        }
+    } else {
+        console.log('Sorry try again!');
+    }
+}
+
+//bonus
+function bonusOne() { }
+
+guessLetter('J');
+guessLetter('U');
+guessLetter('M');
+guessLetter('T');
+guessLetter('P');
