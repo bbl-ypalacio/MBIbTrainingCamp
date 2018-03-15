42a399f9c8aa6315c1407e7e8a698120663f3019
bblypalacio <ypalacio@belizebank.com>
2018-03-15 13:23:36 -0600

    push ass 3 version  1

---
+++ Assigmnets/Ass3/ass3.js
@@ -0,0 +1,56 @@
+function validateCreditCard(ccNum) {
+
+    //Remove dashes from ccNum string
+    var ccNumNoDas = '';
+    for (var i = 0; i < ccNum.length; i++) {
+        if (ccNum[i] !== '-') {
+            ccNumNoDas += ccNum[i];
+        }
+    }
+    // The credit card number must be 16 digits in length
+    if (ccNumNoDas.length !== 16) {
+        return false;
+    }
+    // All of the digits must be numbers
+    for (var i = 0; i < ccNumNoDas.length; i++) {
+        // store the current digit 
+        var currentNumber = ccNumNoDas[i];
+
+        // turn the digit from a string to an integer (if the digit is in fact a digit and not anther char)
+        currentNumber = Number.parseInt(currentNumber);
+
+        // check that the digit is a number
+        if (!Number.isInteger(currentNumber)) {
+            return false;
+        }
+    }
+    // The credit card number must be composed of at least two different digits (i.e. all of the digits cannot be the same)
+    var obj = {};
+    for (var i = 0; i < ccNumNoDas.length; i++) {
+        obj[ccNumNoDas[i]] = true;
+    }
+    if (Object.keys(obj).length < 2) {
+        return false;
+    }
+    // The final digit of the credit card number must be even
+    if (ccNumNoDas[ccNumNoDas.length - 1] % 2 !== 0) {
+        return false;
+    }
+    // The sum of all the digits must be greater than 16
+    var sum = 0;
+    for (var i = 0; i < ccNumNoDas.length; i++) {
+        sum += Number(ccNumNoDas[i]);
+    }
+    if (sum <= 16) {
+        return false;
+    }
+
+    return true;
+};
+
+/**** tests *****/
+console.log(validateCreditCard('9999-9999-8888-0000')); //true
+console.log(validateCreditCard('6666-6666-6666-1666')); //true
+console.log(validateCreditCard('a923-3211-9c01-1112')); //false
+console.log(validateCreditCard('4444-4444-4444-4444')); //false
+console.log(validateCreditCard('1211-1111-1111-1112')); //true
