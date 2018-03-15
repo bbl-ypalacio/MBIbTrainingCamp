e0cc44c132a6826759f05a1348823d79679cb9b5
bblypalacio <ypalacio@belizebank.com>
2018-03-15 13:27:37 -0600

    ass 3 v1

--- Assigmnets/Ass3/ass3.js
+++ Assigmnets/Ass3/ass3.js
@@ -49,8 +49,8 @@ function validateCreditCard(ccNum) {
 };
 
 /**** tests *****/
-console.log(validateCreditCard('9999-9999-8888-0000')); //true
-console.log(validateCreditCard('6666-6666-6666-1666')); //true
-console.log(validateCreditCard('a923-3211-9c01-1112')); //false
-console.log(validateCreditCard('4444-4444-4444-4444')); //false
-console.log(validateCreditCard('1211-1111-1111-1112')); //true
+console.log('9999-9999-8888-0000 Valid status:'+validateCreditCard('9999-9999-8888-0000')); //true
+console.log('6666-6666-6666-1666 Valid status:'+validateCreditCard('6666-6666-6666-1666')); //true
+console.log('a923-3211-9c01-1112 Valid status:'+validateCreditCard('a923-3211-9c01-1112')); //false
+console.log('4444-4444-4444-4444 Valid status:'+validateCreditCard('4444-4444-4444-4444')); //false
+console.log('1211-1111-1111-1112 Valid status:'+validateCreditCard('1211-1111-1111-1112')); //true
