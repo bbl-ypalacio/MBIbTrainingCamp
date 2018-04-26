e886452ebb24fca847de7d7cf99df152a0ccacea
bblypalacio <ypalacio@belizebank.com>
2018-04-26 12:04:02 -0600

    Pushing Webservice

--- Assigmnets/Ass4/WebService/SRC/app.ts
+++ Assigmnets/Ass4/WebService/SRC/app.ts
@@ -1,82 +1,141 @@
 import * as express from 'express';
+import * as _ from 'lodash';
+
+import { InitialData } from './AtmDB/atm.data';
+import { accountList, accountModel, TransactionList, TransactionModel } from './AtmDB/atm.model';
 
 class App {
     public express;
-    public accTxnAmt		: number;
-	public initialBalance 	: number;
-    public currentBalance 	: number;
-    public accountNumber 	: string;
+    
+	private localDB          : accountList;
+    private transactionList  : TransactionList;
 
     constructor() {
-        this.express = express()
-        this.mountRoutes()
+        this.express = express();
+        this.mountRoutes();		
+		/*
+         *  Initialize the local DB
+        */
+          this.localDB = new accountList();
+          this.localDB.accounts = InitialData;
+          this.transactionList = new TransactionList();
     }
 
     private mountRoutes(): void {
-		
-		this.accountNumber="009"; //should be replace(only here to make code work)
         
-        const router                    = express.Router();
+        //const router                    = express.Router();
         const routerIsAliveConnection   = express.Router();
+		const routerAccountExists   	= express.Router();
         const routerSetInitialBalance   = express.Router();
         const routerWithdrawMoney       = express.Router();
         const routerDepositMoney        = express.Router();
-        const routerShowBalance 		= express.Router();
+        const routerShowBalance         = express.Router();
+        const routerGetLastOperations   = express.Router();
 
         //routerIsAliveConnection
-        router.get('/atm', (req, res) => {
-			console.log("isAliveConnection is being called");
+        routerIsAliveConnection.get('/atm/isAliveConnection', (req, res) => {
+            console.log("isAliveConnection is being called");
+			resp.setHeader('Access-Control-Allow-Origin', '*');
             res.json({
                 status	: 0,
                 message	: "Ok"
             })
         });  
+		
+		//routerAccountExists
+        routerAccountExists.get('/atm/AccountExists/:acct', (req, res) => {
+            console.log("routerAccountExists is being called by: " + req.params.acct);
+            let accExist = this.accountExists(req.params.acct);
+			resp.setHeader('Access-Control-Allow-Origin', '*');
+            res.json({
+                status: accExist
+            })
+        });		
 
-        //routerSetInitialBalance
-        router.get('/atm/setInitialBalance/:acct/balance/:bal', (req, res) => {	
+        //routerSetInitialBalance//TODO:modify to alter array
+        routerSetInitialBalance.get('/atm/setInitialBalance/:acct/balance/:bal', (req, res) => {	
 		console.log("routerSetInitialBalance is being called by: " + req.params.acct);		
-			if (req.params.acct == this.accountNumber){//should check JSON array for account number
-				this.accountNumber  = req.params.acct;
-				this.initialBalance = req.params.bal;
-				this.currentBalance = req.params.bal;
-				   				
-				res.json({
-					account: this.accountNumber,
-					amount:  this.initialBalance
-				})
+        if (this.accountExists(req.params.acct)) {
+
+            let acc = req.params.acct;
+            let amt = req.params.bal;
+
+            let result = _.findIndex(this.localDB.accounts, {'accountNumber': acc });
+            this.localDB.accounts[result].currentBalance = amt;
+
+            //record the transaction in the transaction list
+            let newTransaction              = new TransactionModel();
+            newTransaction.accountNumber    = acc;
+            newTransaction.amount           = amt;
+            newTransaction.transactionType  = "SetBalance";
+
+            this.transactionList.transactions.push(newTransaction);
+			
+			resp.setHeader('Access-Control-Allow-Origin', '*');
+            res.json({
+                account : this.localDB.accounts[result].accountNumber,
+                newBal  : this.localDB.accounts[result].currentBalance
+            })
+
 			} else {
 					   console.log('**Error, wrong account number*');
 				   }
 		}); 
 		
 		//routerWithdrawMoney
-		router.get('/atm/withdraw/:account/amount/:amount', (req, res) => {	
-		console.log("routerWithdrawMoney is being called by: " + req.params.account);						
-			if (req.params.account == this.accountNumber ){//should check JSON array for account number
-				this.accTxnAmt 		= req.params.amount;				
-				this.currentBalance = this.currentBalance - this.accTxnAmt;
-				
-				res.json({
-					account: this.accountNumber,
-					amount : this.accTxnAmt,
-					newBal : this.currentBalance
-				})
+        routerWithdrawMoney.get('/atm/withdraw/:account/amount/:amount', (req, res) => {	
+        console.log("routerWithdrawMoney is being called by: " + req.params.account);						
+        if (this.accountExists(req.params.account)) {
+
+            let acc = req.params.account;
+            let amt = req.params.amount; 
+
+            let result = _.findIndex(this.localDB.accounts, {'accountNumber': acc});
+
+            this.localDB.accounts[result].currentBalance -= amt;
+
+            //record the transaction in the transaction list
+            let newTransaction              = new TransactionModel();
+            newTransaction.accountNumber    = acc;
+            newTransaction.amount           = amt;
+            newTransaction.transactionType  = "WithDraw";
+
+            this.transactionList.transactions.push(newTransaction);
+			
+			resp.setHeader('Access-Control-Allow-Origin', '*');
+            res.json({
+                        account : this.localDB.accounts[result].accountNumber,
+                        newBal  : this.localDB.accounts[result].currentBalance
+            })
+
 			}else {
 				  console.log('**Error, wrong account number*');
 			 }
 		});
 		
 		//routerDepositMoney
-        router.get('/atm/deposit/:account/amount/:amount', (req, res) => {			
+        routerDepositMoney.get('/atm/deposit/:account/amount/:amount', (req, res) => {			
 		console.log("routerDepositMoney is being called by: " + req.params.account);			
-			if (req.params.account == this.accountNumber){//should check JSON array for account number				
-				this.accTxnAmt 		= req.params.amount;				
-				this.currentBalance = ((+this.currentBalance) + (+this.accTxnAmt));
+        if (this.accountExists(req.params.account)) {
 
+            let acc = req.params.account;
+            let amt = req.params.amount;
+
+            let result = _.findIndex(this.localDB.accounts, { 'accountNumber': acc });
+            this.localDB.accounts[result].currentBalance = Number(this.localDB.accounts[result].currentBalance) + Number(amt);
+
+            //record the transaction in the transaction list
+            let newTransaction = new TransactionModel();
+            newTransaction.accountNumber = acc;
+            newTransaction.amount = amt;
+            newTransaction.transactionType = "Deposit";
+
+            this.transactionList.transactions.push(newTransaction);
+			
+			resp.setHeader('Access-Control-Allow-Origin', '*');
 				res.json({
-					account: req.params.account,
-					amount : req.params.amount,
-					newBal : this.currentBalance
+                    account : this.localDB.accounts[result].accountNumber,
+                    newBal  : this.localDB.accounts[result].currentBalance
 				})
 			}else {
 				 console.log("**Error, wrong account number*");
@@ -84,24 +143,59 @@ class App {
 		});
 
 		//routerShowBalance
-        router.get('/atm/:account', (req, res) => {
-		console.log("routerShowBalance is being called by: " + req.params.account);
-			if (req.params.account == this.accountNumber){//should check JSON array for account number				
+        routerShowBalance.get('/atm/ShowBalance/:account', (req, res) => {
+            console.log("routerShowBalance is being called by: " + req.params.account);
+            if (this.accountExists(req.params.account)) {
+
+                let result = _.findIndex(this.localDB.accounts, { 'accountNumber': req.params.account });
+				
+				resp.setHeader('Access-Control-Allow-Origin', '*');
 				res.json({
-					account: req.params.account,
-					newBal : this.currentBalance				
+                    account: req.params.account,
+                    newBal: this.localDB.accounts[result].currentBalance				
             })
 			}else {
 				 console.log("**Error, wrong account number*");
 			 }
         });
 
-        this.express.use('/', router)
+        //routerGetLastOperations
+        routerGetLastOperations.get('/atm/GetLastOperations/:account', (req, res) => {
+        console.log("routerGetLastOperations is being called by: " + req.params.account);
+        if (this.accountExists(req.params.account)) {
+
+                let result: TransactionList = new TransactionList();
+
+                result.transactions = _.filter(
+                    this.transactionList.transactions,
+                    {
+                        'accountNumber': req.params.account
+                    }
+                );
+				
+				resp.setHeader('Access-Control-Allow-Origin', '*');
+                res.json({
+                    lastOperations: result
+                })
+            } else {
+                console.log("**Error, wrong account number*");
+            }
+        });
+                
+		this.express.use('/', routerIsAliveConnection)
+		this.express.use('/', routerAccountExists)
 		this.express.use('/', routerSetInitialBalance)
 		this.express.use('/', routerWithdrawMoney)
 		this.express.use('/', routerDepositMoney)
         this.express.use('/', routerShowBalance)
+        this.express.use('/', routerGetLastOperations)
     }
+
+
+    private accountExists(acct: string): boolean {
+        return _.some(this.localDB.accounts, { 'accountNumber': acct });
+    }
+
 }
 
 export default new App().express
