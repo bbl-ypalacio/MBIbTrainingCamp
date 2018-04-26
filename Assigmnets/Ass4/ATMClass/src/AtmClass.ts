70620716b0ce92f62a63fab707487256d8aa8311
bblypalacio <ypalacio@belizebank.com>
2018-04-26 11:58:50 -0600

    testing cnew commit

--- Assigmnets/Ass4/ATMClass/src/AtmClass.ts
+++ Assigmnets/Ass4/ATMClass/src/AtmClass.ts
@@ -1,81 +1,100 @@
 import { AtmInterface } from './AtmInterface';
-import * as got from 'got'; 
+import * as got from 'got';
 
 export class Atm implements AtmInterface {
-	
-	initialBalance 	: number;
-    currentBalance 	: number;
-    accountNumber 	: string;
+
+    initialBalance: number;
+    currentBalance: number;
+    accountNumber: string;
 
     // The constructor for the ATM class only specifies a signature without any code
-    constructor () {
-        
-     }
-	 //TODO:
-	 //isAliveConnection()
-	 //use loadash for db
-	 //Put logic in webervice
+    constructor() {
 
-	 isAliveConnection(){
-		 got.get(`http://localhost:3001/atm`).then (
-			 (result) => {
-				   let data = JSON.parse(result.body);
+    }
+    //TODO:
+    //isAliveConnection()
+    //use loadash for db
+    //Put logic in webervice
 
-				   console.log("\n>>>>isAliveConnection<<<<<<<<");
-				   console.log("Status  is :"+ data.status);
-				   console.log("Message is :"+ data.message);
-			});		 
-	 }
-	 
-    setInitialBalance(acct: string, balance: number): void {                 
-	   got.get(`http://localhost:3001/atm/setInitialBalance/${acct}/balance/${balance}`).then(
-		   (result) => {
-			   let data = JSON.parse(result.body);
+    isAliveConnection(): void {
+        got.get(`http://localhost:3000/atm/isAliveConnection`).then(
+            (result) => {
+                let data = JSON.parse(result.body);
 
-			   console.log("\n>>>>setInitialBalance<<<<<<<<");
-			   console.log("Account Number :" + data.account);
-			   console.log("Initial Balance:" + data.amount);
-			   console.log("Current Balance:" + data.amount);
-	   });
-     }
-    
-    withdrawMoney(acct: string, amount: number): void {	         			 
-			 
-		got.get(`http://localhost:3001/atm/withdraw/${acct}/amount/${amount}`).then( 
-			(result) => {
-				let data = JSON.parse(result.body);               
+                console.log("\n>>>>isAliveConnection<<<<<<<<");
+                console.log("Status  is :" + data.status);
+                console.log("Message is :" + data.message);
+            });
+    }
 
-				console.log("\n>>>>withdrawMoney<<<<<<<<");
-				console.log(" Account Number :" + data.account);
-				console.log(" Withdrew  BZD  :" + data.amount);	
-				console.log(" Avaliable BZD  :" + data.newBal);
-				});         
-     }
+    setInitialBalance(acct: string, balance: number): void {
+        got.get(`http://localhost:3000/atm/setInitialBalance/${acct}/balance/${balance}`).then(
+            (result) => {
+                let data = JSON.parse(result.body);
 
-     depositMoney(acct: string, amount : number) : void {
-         			            
-		got.get(`http://localhost:3001/atm/deposit/${acct}/amount/${amount}`).then((result) => {
-			let data = JSON.parse(result.body);
+                console.log("\n>>>>setInitialBalance<<<<<<<<");
+                console.log("Account Number :" + data.account);
+                console.log("Initial Balance:" + data.newBal);
+            });
+    }
 
-			console.log("\n>>>>depositMoney<<<<<<<<");
-			console.log("Account Number   :"+data.account);
-			console.log("Amount deposited :"+data.amount);
-			console.log("Avaliable balance:"+data.newBal);
-		});
-         
-     }
+    withdrawMoney(acct: string, amount: number): void {
+        got.get(`http://localhost:3000/atm/withdraw/${acct}/amount/${amount}`).then(
+            (result) => {
+                let data = JSON.parse(result.body);
 
-     showBalance(acct:string) :void {		
-		 got.get(`http://localhost:3001/atm/${acct}`).then( (result) => {
-			let data = JSON.parse(result.body);
-			
-		  console.log("\n>>>>showBalance<<<<<<<<");
-		  console.log("Account Number  :" + data.account);
-		  console.log("Current Balance :" + data.newBal);
-		}); 
-     }
-   
-     closeOperation() : void {
-         this.initialBalance = this.currentBalance ;
-     }
+                console.log("\n>>>>withdrawMoney<<<<<<<<");
+                console.log(" Account Number :" + data.account);
+                console.log(" Avaliable BZD  :" + data.newBal);
+            });
+    }
+
+    depositMoney(acct: string, amount: number): void {
+        got.get(`http://localhost:3000/atm/deposit/${acct}/amount/${amount}`).then((result) => {
+            let data = JSON.parse(result.body);
+
+            console.log("\n>>>>depositMoney<<<<<<<<");
+            console.log("Account Number   :" + data.account);
+            console.log("Avaliable balance:" + data.newBal);
+        });
+    }
+
+    showBalance(acct: string): void {
+        got.get(`http://localhost:3000/atm/ShowBalance/${acct}`).then((result) => {
+            let data = JSON.parse(result.body);
+
+            console.log("\n>>>>showBalance<<<<<<<<");
+            console.log("Account Number  :" + data.account);
+            console.log("Current Balance :" + data.newBal);
+        });
+    }
+
+    accountExists(acct: string): void {
+        got.get(`http://localhost:3000/atm/AccountExists/${acct}`).then((result) => {
+            let data = JSON.parse(result.body);
+
+            console.log("\n>>>>accountExists<<<<<<<<");
+            console.log("accountExists?  :" + data.status);
+        });
+    }
+
+    getLastOperations(acct: string): void {
+        got.get(`http://localhost:3000/atm/GetLastOperations/${acct}`).then((result) => {
+            let data = JSON.parse(result.body);
+
+            console.log("\n>>>>getLastOperations<<<<<<<<");
+            console.log("LastOperations  :" + data.lastOperations);
+        });
+    }
+
+    closeOperation(): void {
+        console.log("\n>>>>closeOperation<<<<<<<<");
+        this.initialBalance = this.currentBalance;
+    }
+
+
+
+
+
+
 }
--- Assigmnets/Ass4/ATMClass/src/atm.data.ts
+++
@@ -1,5 +0,0 @@
-export var InitialData = [
-    { accountNumber : '23232-1', currentBalance : 500},
-    { accountNumber : '23232-2', currentBalance : 1500},
-    { accountNumber : '23232-3', currentBalance : 300}
-];
