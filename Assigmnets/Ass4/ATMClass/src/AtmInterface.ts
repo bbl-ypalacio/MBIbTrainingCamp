a3527be4bc1d9cb28f977fa3fa4f7172bf219d42
bblypalacio <ypalacio@belizebank.com>
2018-04-26 12:02:28 -0600

    ATMclass

--- Assigmnets/Ass4/ATMClass/src/AtmInterface.ts
+++ Assigmnets/Ass4/ATMClass/src/AtmInterface.ts
@@ -3,39 +3,21 @@ export interface AtmInterface {
     /*
     *   These the encapsulated data of the ATM
     */
-    initialBalance : number,
-    currentBalance : number,
-    accountNumber  : string,
-
+    initialBalance: number,
+    currentBalance: number,
+    accountNumber: string,
 
     /* 
     *   Method´s definition
     */
 
-	isAliveConnection(): void,
-    setInitialBalance(acct:string, balance : number ) : void,
-    withdrawMoney(acct: string, amount : number ) : void,
-    depositMoney(acct: string, amount : number) : void,
-    showBalance(acct:string) :void,
-    closeOperation() : void
+    isAliveConnection(): void,
+    setInitialBalance(acct: string, balance: number): void,
+    withdrawMoney(acct: string, amount: number): void,
+    depositMoney(acct: string, amount: number): void,
+    showBalance(acct: string): void,
+    accountExists(acct: string): void,
+    getLastOperations(acct: string): void,
+    closeOperation(): void
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- Assigmnets/Ass4/ATMClass/src/index.ts
+++ Assigmnets/Ass4/ATMClass/src/index.ts
@@ -1,10 +1,12 @@
 import { Atm } from './AtmClass';
 
-var atm : Atm = new Atm();
+var atm: Atm = new Atm();
 
 atm.isAliveConnection();
-atm.setInitialBalance("009",500);
-atm.withdrawMoney("009",50);
-atm.depositMoney("009",50);
-atm.showBalance("009");
+atm.setInitialBalance("23232-3", 500);
+atm.withdrawMoney("23232-3", 100);
+atm.depositMoney("23232-3", 50);
+atm.showBalance("23232-3");//not working
+atm.accountExists("23232-3");//not working
+//atm.getLastOperations("23232-3");
 atm.closeOperation();
