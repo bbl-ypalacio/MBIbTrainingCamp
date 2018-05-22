c423e808ecda474d6d2e825a7dfc20ded489a3b2
bblypalacio <ypalacio@belizebank.com>
2018-05-22 06:38:44 -0600

    lab06 v0

--- Assigmnets/Ass7/lab06/src/app/app.module.ts
+++ Assigmnets/Ass7/lab06/src/app/app.module.ts
@@ -9,28 +9,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
+import { RoutingModule } from './router.module';
 
 import { AtmServiceService } from './services/atm-service.service';
+import { GuardServiceService } from './services/guard-service.service';
+
 import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
 import { AtmoperationsComponent } from './components/atmoperations/atmoperations.component';
 import { AuthenticateComponent } from './components/authenticate/authenticate.component';
-import { componentFactoryName } from '@angular/compiler';
 import { MainopComponent } from './components/mainop/mainop.component';
 
-const myRoutes : Routes = [ 
-
-    { path : 'auth' , component : AuthenticateComponent },
-    { path : '' , redirectTo : '/auth', pathMatch : 'full' },
-    { path : 'mainoperation' , component : MainopComponent}, 
-
-    { path : 'transactionlist' , component : TransactionListComponent,
-      outlet : 'insideatm' },
-
-    { path : 'operations' , component : AtmoperationsComponent,
-      outlet : 'insideatm' }   
-     
-];
+ 
 
 @NgModule({
   declarations: [
@@ -44,12 +33,12 @@ const myRoutes : Routes = [
   imports: [
     BrowserModule  ,
     HttpClientModule,
-    RouterModule.forRoot(myRoutes),
+    RoutingModule,
     ReactiveFormsModule
   ],
   //We have to import the service as a provider in the NGModule ecosystem
   //otherwise it won´t be available in the Angular Application
-  providers: [AtmServiceService],
+  providers: [AtmServiceService,GuardServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Assigmnets/Ass7/lab06/src/app/components/atmoperations/atmoperations.component.ts
+++ Assigmnets/Ass7/lab06/src/app/components/atmoperations/atmoperations.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, AfterContentChecked, AfterContentInit } from '@angular/core';
 import { AtmServiceService } from '../../services/atm-service.service';
 import { AtmResponseOperation,AtmResponseOperationBalance } from '../../models/atm.interface';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-atmoperations',
   templateUrl: './atmoperations.component.html',
   styleUrls: ['./atmoperations.component.css']
 })
-export class AtmoperationsComponent implements OnInit {
+export class AtmoperationsComponent implements OnInit{
   
-  public currentOperation   : string = '';
   public currentBalance     : number = 0;
   public atmResponse        : AtmResponseOperation = <AtmResponseOperation>{}; 
   public atmResponseBalance : AtmResponseOperationBalance = <AtmResponseOperationBalance>{};
+  public title              : string = '';
+  
+  private amount            : number = 0;
+  private operation         : number = 0;
+
+  public theForm    : FormGroup;
 
   constructor(public atmService : AtmServiceService,
   public route : ActivatedRoute) { }
 
+ 
   ngOnInit() {
  
         let acctNumber = this.atmService.getAccountNumber();
@@ -27,29 +34,40 @@ export class AtmoperationsComponent implements OnInit {
             console.log("received parameter",data['arg']);
 
                switch (data['arg']) {
-                     case 'deposit'   : { this.makeADeposit(acctNumber,100); break }
-                     case 'withdrawl' : { this.makeAWithdraw(acctNumber,50); break }
+                     case 'deposit'   : { this.operation = 1; this.title = "Deposit"; break }
+                     case 'withdrawl' : { this.operation = 2; this.title = "Witdraw"; break }
                      
                }
         });
+
+        this.theForm = new FormGroup({
+              amount : new FormControl(0,Validators.required)
+        });
+
+        this.atmService.onUpdatedTransactions$.subscribe(data=>{
+
+              this.theForm.setValue({amount:0});
+
+         });
+
            
   }
 
-  showBalance(acct:string) {
+  doOperation(mickey:FormGroup) {
 
-    this.currentOperation = 'Querying Balance';
+    this.amount =  mickey.value.amount;
 
-    this.atmService.getCurrentBalance(acct).subscribe(result => {
-            this.atmResponseBalance = result;
-            this.currentBalance = result.currentBalance;
-      });
-
+    if (this.operation == 1 ) {
+           this.makeADeposit(this.atmService.getAccountNumber(),this.amount);   
+    }
+    else {
+          this.makeAWithdraw(this.atmService.getAccountNumber(),this.amount);
+    }
   }
 
+
   makeADeposit(acct:string,amount:number) {
-
-    this.currentOperation = "Making a Deposit";
-
+ 
     this.atmService.deposit(acct,amount).then( result => {
                this.atmResponse = result;
                this.currentBalance = result.currentBalance;
@@ -58,8 +76,6 @@ export class AtmoperationsComponent implements OnInit {
   }
 
   makeAWithdraw(acct:string,amount:number) {
-
-       this.currentOperation = "Making a Withdrawl";
 
         this.atmService.withDraw(acct,amount).then( result => {
                 this.atmResponse = result;
